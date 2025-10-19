9c75a16a677f736ab4b628380470be309044a0a1
Milton Lima <milton.mlima@gmail.com>
2025-10-19 11:24:25 -0300

    Definir valor padrão para a propriedade Nome na classe Turma e ajustar a seleção de modalidade na página de edição para melhorar a usabilidade.

--- Models/Turma.cs
+++ Models/Turma.cs
@@ -13,7 +13,7 @@ namespace MvcMovie.Models
         [Display(Name = "Nome")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 3)]
-        public string Nome { get; set; }
+        public string Nome { get; set; } = string.Empty;
 
         [Display(Name = "Data de Início")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
